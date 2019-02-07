8d4b376ae0d4fce70f84cb0880b489e77fa01be7
James Vidler <james.vidler@outlook.com>
2019-02-07 13:06:55 -0500

    added youtube video, updated fonts, and added bg

--- src/App.js
+++ src/App.js
@@ -2,25 +2,50 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import './sass/mystyles.scss'
+import YouTube from 'react-youtube'
 
 class App extends Component {
   render() {
+    const opts = {
+        height: '390',
+        width: '640',
+        playerVars: { // https://developers.google.com/youtube/player_parameters
+          autoplay: 0
+        }
+      };
+
     return (
       <div className="App">
-        <section class="hero is-info is-fullheight">
+        <section class="hero is-fullheight">
             <div class="hero-head">
-              <h1 className="title pageTitle">dodgeblock VR</h1>
+              <h1 className="title pageTitle">Dodgeblock VR</h1>
             </div>
 
             <div class="hero-body">
                 <div class="container has-text-centered">
-                    <div class="column is-6 is-offset-3">
+                    <div class="main-column column is-two-thirds">
+                    
+                        <div className="youtube-container">
+                            <div className="youtube">
+                                <YouTube
+                                    videoId="TjfY_cBc8DI"
+                                    opts={opts}
+                                    onReady={this._onReady}
+                                />
+                            </div>
+                        </div>
+
                         <h1 class="title">
-                            Coming Soon
+                            Coming soon...
+                        </h1>
+
+                        <h1 class="title">
+                            An endless runner, re-imagined in VR.
                         </h1>
                         <h2 class="subtitle">
-                            An endless runner, re-imagined in VR. <br/> Fly through a retro-arcade inspired environment using 3DOF.
+                            No two runs are ever the same. How long can you last?
                         </h2>
+                        <p>&nbsp;</p>
                         <div class="box">
                             Plaforms: Oculus Go, Rift (future)
                         </div>
